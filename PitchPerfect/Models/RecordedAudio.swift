7462b1fdae123a2df5efb258edf013cc47682bb1
Aaron Pankratz <pankratz.aaron@gmail.com>
2015-10-04 09:42:41 -0500

    Add RecordedAudio model class

---
+++ PitchPerfect/Models/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  PitchPerfect
+//
+//  Created by Aaron Pankratz on 10/4/15.
+//  Copyright © 2015 Aaron Pankratz. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
